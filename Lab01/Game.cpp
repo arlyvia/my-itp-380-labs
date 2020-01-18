c015784af5492763d26fe3c3c6b7c36bcf4b9222
Arlyvia Chaialee <chaialee@usc.edu>
2020-01-18 14:25:58 -0800

    Completed Part 1
    Paddle, walls, and ball are all there

--- Lab01/Game.cpp
+++ Lab01/Game.cpp
@@ -37,8 +37,22 @@ void Game::Shutdown(){
     
 }
 
+void Game::ProcessInput(){
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                runLoop = false;
+        }
+    }
+    const Uint8 *state = SDL_GetKeyboardState(NULL);
+    if (state[SDL_SCANCODE_ESCAPE]) {
+        runLoop = false;
+    }
+}
+
 void Game::RunLoop(){
-    while(true){
+    while(runLoop){
         ProcessInput();
         UpdateGame();
         GenerateOutput();
@@ -47,4 +61,37 @@ void Game::RunLoop(){
 
 void Game::UpdateGame(){}
 
-void Game::GenerateOutput(){}
+void Game::MakeARect(SDL_Rect wall, SDL_Renderer* renderer,
+               int x1, int y1, int w1, int h1){
+    wall.x = x1;
+    wall.y = y1;
+    wall.w = w1;
+    wall.h = h1;
+    SDL_RenderFillRect(renderer, &wall);
+}
+
+void Game::GenerateOutput(){
+    SDL_SetRenderDrawColor(myRenderer, 0, 0, 255, 255);
+    SDL_RenderClear(myRenderer);
+    
+    SDL_SetRenderDrawColor(myRenderer, 255, 255, 255, 255);
+    SDL_Rect top_wall;
+    SDL_Rect side_wall;
+    SDL_Rect bottom_wall;
+    const int thickness = 20;
+    MakeARect(top_wall, myRenderer, 0, 0, 1024, thickness);
+    MakeARect(side_wall, myRenderer, 1004, 0, thickness, 768);
+    MakeARect(bottom_wall, myRenderer, 0, 748, 1024, thickness);
+    
+    SDL_SetRenderDrawColor(myRenderer, 255, 50, 50, 255);
+    SDL_Rect paddle;
+    const int paddle_height = 100;
+    MakeARect(paddle, myRenderer, 15, 324, 12, paddle_height);
+    
+    SDL_SetRenderDrawColor(myRenderer, 90, 255, 0, 255);
+    SDL_Rect ball;
+    const int ball_height = 15;
+    MakeARect(ball, myRenderer, 512, 384, ball_height, ball_height);
+    
+    SDL_RenderPresent(myRenderer);
+}
--- Lab01/Game.h
+++ Lab01/Game.h
@@ -12,24 +12,23 @@ class Game {
         SDL_Window* myWindow;
         SDL_Renderer* myRenderer;
     
-        bool runLoop;
+        bool runLoop = true;
+    
+        SDL_Point paddlePosition = {20, 384};
+    
+        SDL_Point ballPosition = {512, 384};
     
         //functions
         bool Initialize();
         void Shutdown();
         void RunLoop();
     
-        void ProcessInput(){
-            SDL_Event event;
-            while (SDL_PollEvent(&event)) {
-                switch (event.type) {
-                    case SDL_QUIT: runLoop = false;
-                }
-            }
-        }
+        void ProcessInput();
         void UpdateGame();
         void GenerateOutput();
         
+        void MakeARect(SDL_Rect wall, SDL_Renderer* renderer,
+                       int x1, int y1, int w1, int h1);
 };
 
 
