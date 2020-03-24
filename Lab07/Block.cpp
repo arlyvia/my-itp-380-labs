ffbe72b4f456f7d82407388f9dd7bb6ae48733bd
Arlyvia Chaialee <chaialee@usc.edu>
2020-03-24 02:30:24 -0700

    explode!!!

--- Lab07/Block.cpp
+++ Lab07/Block.cpp
@@ -36,14 +36,26 @@ void Block::OnUpdate(float deltaTime){
 }
 
 void Block::explode(){
+    Vector3 original_block_pos = this->GetPosition();
+    GetGame()->RemoveBlock(this);
+    SetState(ActorState::Destroy);
+    
+    std::vector<Block*> toDestroy;
+    
     for(int i = 0; i < (int)(unsigned)GetGame()->mBlocks.size(); i++){
-        if(this != GetGame()->mBlocks[i] && GetGame()->mBlocks[i]->mExploding == true){
-            Vector3 temp = this->GetPosition() - GetGame()->mBlocks[i]->GetPosition();
-            float distance = sqrt(temp.x*temp.x + temp.y*temp.y + temp.z*temp.z);
-            if(distance <= 50.0f){
-                GetGame()->mBlocks[i]->SetState(ActorState::Destroy);
-                SetState(ActorState::Destroy);
-            }
+        Vector3 temp = original_block_pos - GetGame()->mBlocks[i]->GetPosition();
+        float distance = sqrt(temp.x*temp.x + temp.y*temp.y + temp.z*temp.z);
+        if(distance <= 50.0f){
+            toDestroy.push_back(GetGame()->mBlocks[i]);
+        }
+    }
+    
+    for(int i=0; i < (int)(unsigned)toDestroy.size(); i++){
+        if(toDestroy[i]->mExploding == true){
+            toDestroy[i]->explode();
+        } else {
+            toDestroy[i]->SetState(ActorState::Destroy);
+            GetGame()->RemoveBlock(toDestroy[i]);
         }
     }
 }
