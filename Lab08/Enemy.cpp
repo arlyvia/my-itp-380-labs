bc9534aeb4c025b85c5bc07c9afbc944e2322631
Arlyvia Chaialee <chaialee@usc.edu>
2020-03-26 20:09:35 -0700

    travis

--- Lab08/Enemy.cpp
+++ Lab08/Enemy.cpp
@@ -6,3 +6,18 @@
 //
 
 #include "Enemy.hpp"
+#include "Game.h"
+#include "MeshComponent.h"
+#include "Renderer.h"
+#include "EnemyMove.hpp"
+
+Enemy::Enemy(class Game* game)
+:Actor(game)
+{
+    enemy_mhc = new MeshComponent(this);
+    enemy_mhc->SetMesh(GetGame()->GetRenderer()->GetMesh("Assets/Kart.gpmesh"));
+    enemy_mhc->SetTextureIndex(6);
+    SetScale(0.75f);
+    
+    enemy_move = new EnemyMove(this);
+}
--- Lab08/Enemy.hpp
+++ Lab08/Enemy.hpp
@@ -9,5 +9,19 @@
 #define Enemy_hpp
 
 #include <stdio.h>
+#include "Actor.h"
+#include "Math.h"
+
+class Enemy: public Actor {
+    public:
+        //constructor
+        Enemy(class Game* game);
+        
+        class MeshComponent* enemy_mhc;
+        class EnemyMove* enemy_move;
+    
+        std::vector<Vector3>
+};
+
 
 #endif /* Enemy_hpp */
